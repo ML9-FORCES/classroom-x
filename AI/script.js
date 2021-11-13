115e2857946f1abca8ef27d44aeb3dc818031ae6
Chelsi-create <chelsiworld@gmail.com>
2021-11-13 18:55:42 +0530

    Face detection functionality added

--- AI/script.js
+++ AI/script.js
@@ -12,3 +12,35 @@ const setupCamera = () => {
         video.srcObject = stream;
     });
 };
+
+const detectFaces = async () => {
+    const prediction = await model.estimateFaces(video, false);
+
+    console.log(prediction);
+
+    ctx.drawImage(video, 0, 0, 600, 400);
+
+    prediction.forEach((pred) => {
+        ctx.beginPath();
+        ctx.lineWidth = "4";
+        ctx.strokeStyle = "blue";
+        ctx.rect(
+            pred.topLeft[0],
+            pred.topLeft[1],
+            pred.bottomRight[0] - pred.topLeft[0],
+            pred.bottomRight[1] - pred.topLeft[1]
+        );
+        ctx.stroke();
+
+        ctx.fillStyle = "red";
+        pred.landmarks.forEach((landmark) => {
+            ctx.fillRect(landmark[0], landmark[1], 5, 5)
+        });
+    });
+}
+
+setupCamera();
+video.addEventListener("loadeddata", async () => {
+    model = await blazeface.load();
+    setInterval(detectFaces, 40);
+});
